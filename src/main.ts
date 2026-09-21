582ec468a2b822e4af08a1ebcf70d5631d05b6b4
Josh <joshpatg2005@gmail.com>
2026-09-21 11:45:33 +0100

    fix docker compose not connecting api to db

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.APP_PORT ?? 6767);
+  await app.listen(process.env.APP_PORT!);
 }
 void bootstrap();
