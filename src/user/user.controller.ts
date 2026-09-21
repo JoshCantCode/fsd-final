98bf413bda5b3f5e80eeb5f3fd374e3e7a685a59
Josh <joshpatg2005@gmail.com>
2026-09-21 12:19:07 +0100

    fix returns of user controller and proper creation of user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 import type CreateUserDto from "src/dtos/create-user.dto";
 
@@ -12,7 +12,7 @@ export class UserController {
   }
 
   @Get(":id")
-  async getUser(id: string) {
+  async getUser(@Param("id") id: string) {
     return await this.userService.getUser(id);
   }
 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -13,21 +13,40 @@ export class UserService {
   ) {}
 
   async getUsers(): Promise<User[]> {
-    return await this.userRepository.find();
+    return await this.userRepository.find({
+      relations: {
+        bookings: true,
+        billing: true,
+      },
+    });
   }
 
   async getUser(id: string): Promise<User> {
     return await this.userRepository.findOneOrFail({
       where: { id },
+      relations: {
+        bookings: true,
+        billing: true,
+      },
     });
   }
 
   async createUser({ name, email }: CreateUserDto) {
+    // create billing object
     const billing = new Billing();
-    return await this.userRepository.insert({
+    await this.userRepository.manager.save(billing);
+
+    // create user and add the billing object to it
+    const user = new User();
+    user.billing = billing;
+    user.name = name;
+    user.email = email;
+    await this.userRepository.manager.save(user);
+
+    return {
       name,
       email,
       billing,
-    });
+    };
   }
 }
